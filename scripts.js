da15b87549d50ffce91871b72862b1d2acb3de87
c <cmrice@ucsd.edu>
2023-11-30 19:05:43 -0800

    Added msg for errors

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 
 /**
- * 6 Add flashing for comments
+ * TODO:
+ * 1) Make color wheel
+ * 2) Make output messages with output tag
+ * 3) Make error message display the incorrect character
  */
 const nameElem = document.getElementById("name");
 const emailElem = document.getElementById("email");
@@ -57,36 +60,44 @@ runEventListeners();
 
 function evalName() {
     if (nameChars.test(nameElem.value)) {
+        setErrorMsg("name-error", "Error: Cannot use special characters");
         initiateFlash(nameElem);
         updateErrorArray();
-        nameElem.setCustomValidity("Name did not validate");
+        nameElem.setCustomValidity("Cannot use special characters");
     } else {
+        clearErrorMsg("name-error");
         nameElem.setCustomValidity("");
     }
 }
 function evalEmail() {
     if (emailElem.validity.typeMismatch) {
+        setErrorMsg("email-error", "Error: Email is invalid");
         updateErrorArray();
         emailElem.setCustomValidity("Email is invalid");
     } else {
+        clearErrorMsg("email-error");
         emailElem.setCustomValidity("");
     }
 }
 
 function evalComments() {
     if (commentElem.validity.tooLong) {
+        setErrorMsg("comments-error", "Error: Comments are too long");
         updateErrorArray();
         commentElem.setCustomValidity("Comments are too long");
     } 
     else if (commentElem.validity.tooShort) {
+        setErrorMsg("comments-error", "Error: Comments are too short");
         updateErrorArray();
         commentElem.setCustomValidity("Comments are too short");
     }
     else if (specialChars.test(commentElem.value)) {
+        setErrorMsg("comments-error", "Error: Comments cannot use special characters");
         initiateFlash(commentElem);
         updateErrorArray();
         commentElem.setCustomValidity("Comments cannot use special characters");
     } else {
+        clearErrorMsg("comments-error");
         commentElem.setCustomValidity("");
     }
 }
@@ -113,17 +124,24 @@ function updateErrorArray() {
     let name = nameElem.value;
     let email = emailElem.value;
     let comments = commentElem.value;
-    form_errors.push({name, email, comments})
-    // console.log(form_errors);
+    form_errors.push({name, email, comments});
 }
 function clearArray() {
     while (form_errors.length > 0) {
         form_errors.pop();
     }
 }
+function setErrorMsg(stringName, errorMsg) {
+    let currElem = document.getElementById(stringName);
+    currElem.innerHTML = errorMsg;
+}
+function clearErrorMsg(stringName) {
+    let currElem = document.getElementById(stringName);
+    currElem.innerHTML = "";
+}
 function initiateFlash(elem) {
     let i = 0;
-    var setID = setInterval(function() {
+    let setID = setInterval(function() {
         flash(elem);
         console.log(i);
         (i == 5) ? clearInterval(setID) : i=i+1;
@@ -138,8 +156,6 @@ function flash(elem) {
     }
 }
 
-// 
-
 function setTheme(theme) {
     const root = document.documentElement;
     console.log("Script gets here");
@@ -147,11 +163,13 @@ function setTheme(theme) {
         root.style.setProperty('--dark-drab', 'var(--dark-dark-theme)'); 
         root.style.setProperty('--light-drab', 'var(--light-dark-theme)');
         root.style.setProperty('--dark-white', 'var(--deep-red-theme)');
+        root.style.setProperty('background', 'var(--deep-red-theme)')
         localStorage.setItem("theme", "dark");
     } else {
         root.style.setProperty('--dark-drab', 'var(--dark-drab-theme)'); 
         root.style.setProperty('--light-drab', 'var(--light-drab-theme)');
         root.style.setProperty('--dark-white', 'var(--dark-white-theme)');
+        root.style.setProperty('background', 'white')
         localStorage.setItem("theme", "light");
     }
 }
